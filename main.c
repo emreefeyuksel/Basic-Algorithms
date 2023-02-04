2abcef08009d863be8f9fe9ae79cc9e7aa5a43ba
emreefeyuksel <115290667+emreefeyuksel@users.noreply.github.com>
2023-02-04 11:22:48 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int arr[]={9,2,8,5,1,6,7,4,3};
+int size=9;
+
+void print_arr(){
+    for (int i = 0; i < size; ++i) {
+        printf("%d ",arr[i]);
+    }
+}
+
+void find_pair(int x){
+    int k=0;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if((arr[i]+arr[j])==x && i!=j){
+                k++;
+                printf("%d. pair:[%d,%d]\n",k,arr[i],arr[j]);
+            }
+        }
+    }
+    if(k==0){printf("pair not found!");}
+}
+int main() {
+    int target;
+    print_arr();
+    printf("\nInsert the target to find pairs.");
+    scanf("%d",&target);
+    find_pair(target);
+    return 0;
+}
