456253d3ace98d2ea7ccc0d7850f77a07d3a063e
emreefeyuksel <115290667+emreefeyuksel@users.noreply.github.com>
2023-02-04 11:30:54 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define N 200
+
+int main() {
+    int arr[N];
+    int p=1;
+    while(p<N){
+        p++;
+        int j=2*p;
+        while(j<N) {
+            arr[j]=1;
+            j+=p;
+        }
+    }
+    for (int i = 2; i < N; ++i) {
+        if(arr[i]!=1)
+            printf("%d ",i);
+    }
+    return 0;
+}
