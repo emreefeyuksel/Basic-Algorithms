a34a1e9583a4edbe844695e213926e43b719d3ea
emreefeyuksel <115290667+emreefeyuksel@users.noreply.github.com>
2023-02-04 11:26:19 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#define size 10
+float calSD(int arr[size]){
+    float sum=0,varsum=0;
+      for (int i=0;i<size;i++){
+          sum+=arr[i];
+      }
+    float avg=sum/size;
+    for (int i=0;i<size;i++){
+        varsum+=pow((arr[i]-avg),2);
+    }
+    return sqrt(varsum/size);
+}
+int main( )
+{   int arr[size];
+    printf("Please enter %d numbers to calculate Standart deviation of them.\n",size);
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("\nStandart Deviation = %f",calSD(arr));
+    return 0;
+}
